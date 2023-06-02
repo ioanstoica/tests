9ebaf6b309f74a64a63858796a0ec764ece18f07
ioanstoica <ioanstoica89@gmail.com>
2023-06-02 03:28:12 +0300

    angular without doc

--- web_angular_node/MDSfrontend2/src/app/app.module.ts
+++ web_angular_node/MDSfrontend2/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FcComponent } from './fc/fc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FcComponent
   ],
   imports: [
     BrowserModule,
---
+++ web_angular_node/MDSfrontend2/src/app/fc/fc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fc',
+  templateUrl: './fc.component.html',
+  styleUrls: ['./fc.component.scss']
+})
+export class FcComponent {
+
+}
